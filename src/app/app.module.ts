e8a0e5513ab1f7a15f062731bfd75c2cc97ae570
aldaine-AG <aldaineclarke1@gmail.com>
2022-07-08 17:04:19 -0500

    removed the menu components and updated the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './Pages/home/home.component';
 import { AboutComponent } from './Pages/about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -18,18 +17,15 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { DetailsComponent } from './Pages/details/details.component';
 import { ContactComponent } from './Pages/contact/contact.component';
 import { OrderCompletedComponent } from './Pages/order-completed/order-completed.component';
-import { MenuComponent } from './Pages/menu/menu.component';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MenuDetailsComponent } from './Pages/menu-details/menu-details.component';
-import { UpdateMenuComponent } from './Pages/update-menu/update-menu.component';
 import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
-import { SliderComponent } from './slider/slider.component';
-import { NextDirective } from './next.directive';
-import { PreviousDirective } from './previous.directive';
-// import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './Components/slider/slider.component';
+import { NextDirective } from './Directives/next.directive';
+import { PreviousDirective } from './Directives/previous.directive';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -40,13 +36,9 @@ import { PreviousDirective } from './previous.directive';
     ContactComponent,
     RatingComponent,
     OrderCompletedComponent,
-    MenuComponent,
-    MenuDetailsComponent,
-    UpdateMenuComponent,
     SliderComponent,
     NextDirective,
     PreviousDirective,
-    // FooterComponent,
   ],
   imports: [
     BrowserModule,
