a668e34c3c1c71824984eee32d00c445d9fdfbc1
aldaine-AG <aldaineclarke1@gmail.com>
2022-07-08 16:22:02 -0500

    put slider in the components and added directive into Directives folder

--- src/app/previous.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PreviousDirective } from './previous.directive';
-
-describe('PreviousDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PreviousDirective();
-    expect(directive).toBeTruthy();
-  });
-});
