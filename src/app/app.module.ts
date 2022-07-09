4d0c460cf072b3931913b2193bb99b37bafaacf2
HumbleTheo <romariodonald1000@gmail.com>
2022-07-08 21:31:07 -0500

    Adding map component to Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
+// import { MapComponent } from './Components/map/map.component';
 // import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
@@ -40,6 +41,7 @@ import { MatInputModule } from '@angular/material/input';
     MenuComponent,
     MenuDetailsComponent,
     UpdateMenuComponent,
+    // MapComponent,
   ],
   imports: [
     BrowserModule,
