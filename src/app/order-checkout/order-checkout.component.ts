e101f1749d8fe3ae833c8f2ac605364de1b469ec
aldaine-AG <aldaineclarke1@gmail.com>
2022-07-14 01:35:08 -0500

    updated checkout page to show order details

--- src/app/order-checkout/order-checkout.component.ts
+++ src/app/order-checkout/order-checkout.component.ts
@@ -7,7 +7,11 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserCreationComponent } from '../Components/user-creation/user-creation.component';
 import { cardPayment } from '../interfaces/cardPayment';
+import { CartItem } from '../interfaces/cartItem';
+import { SidesInterface } from '../interfaces/sides';
 import { User } from '../interfaces/users';
+import { CartService } from '../Services/cart.service';
+import { DataService } from '../Services/data.service';
 import { UserService } from '../Services/user.service';
 @Component({
   selector: 'app-checkout',
@@ -18,7 +22,8 @@ export class OrderCheckoutComponent implements OnInit {
 
   @ViewChild('stepper') stepper!:MatStepper;
 
-
+  cartItems:CartItem[] = this.cartService.getCart();
+  sides!:SidesInterface[] ;
   currentUID = 0;
   searchUserForm = this._formBuilder.group({
     email:['', Validators.required]
@@ -27,7 +32,7 @@ export class OrderCheckoutComponent implements OnInit {
   paymentForm = this._formBuilder.group({
     selectedCard : ['', Validators.required],
     addNewCard:this._formBuilder.group({
-      cardholderName: [''],
+      cardholder: [''],
       cardNumber: [''],
       cardType: [''],
       expiryDate: [''],
@@ -37,7 +42,7 @@ export class OrderCheckoutComponent implements OnInit {
   isEditable = true;
   currentUser$!: Observable<User | null>;
 
-  constructor(private _formBuilder: FormBuilder,private userService: UserService,    public dialog: MatDialog,private router: Router) {}
+  constructor(private _formBuilder: FormBuilder,private userService: UserService,    public dialog: MatDialog,private router: Router, private cartService: CartService, private dataService: DataService) {}
 
 
   openDialog() {
@@ -55,10 +60,10 @@ export class OrderCheckoutComponent implements OnInit {
         }
         let newUser:User = {
           id: this.currentUID,
-          firstName: result.firstName,
-          lastName: result.lastName,
+          firstName: result.fname,
+          lastName: result.lname,
           email: result.email,
-          address : result.addressL1 +","+result.parish,
+          address : result.address1 +","+result.parish,
           paymentMethod:Array.of(paymentMethod)
         }
         this.userService.saveUser(newUser).subscribe(()=>{
@@ -80,8 +85,11 @@ export class OrderCheckoutComponent implements OnInit {
           this.openDialog();
   
         } else {
-          this.userService.login(result as User);
+          this.userService.login(result as User).subscribe();
+          this.currentUser$ = this.userService.user$
+          console.log(result)
           this.stepper.next()
+
         }
       });
     }else{
@@ -94,6 +102,22 @@ export class OrderCheckoutComponent implements OnInit {
   next() {
   
   }
+  addCard(){
+    let card:cardPayment = {
+      name: this.paymentForm.get("addNewCard")?.get("cardholder")?.value,
+      cardNumber: this.paymentForm.get("addNewCard")?.get("cardNumber")?.value,
+      expiryDate: this.paymentForm.get("addNewCard")?.get("expiryDate")?.value
+    }
+
+    this.currentUser$.subscribe((user)=>{
+      if(user){
+        user.paymentMethod.push(card);
+        console.log(user)
+        this.userService.updateUser(user,user.id).subscribe()
+      }
+
+    })
+  }
   
 
   ngOnInit(): void {
@@ -102,7 +126,11 @@ export class OrderCheckoutComponent implements OnInit {
     this.userService.lastUserId.subscribe((id)=>{
       this.currentUID = id + 1;
     })
-    this.currentUser$ = this.userService.user$
+    this.currentUser$ = this.userService.user$;
+    this.dataService.getAllSides().subscribe((sides)=>{
+      this.sides = sides;
+    })
+
   }
 
 }
