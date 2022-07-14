db8b363fbcad71dd13f91e6c2281c07cb79c7a25
aldaine-AG <aldaineclarke1@gmail.com>
2022-07-13 21:52:09 -0500

    updated the user-creation component with reactive form to collect userInputs

--- src/app/Components/user-creation/user-creation.component.ts
+++ src/app/Components/user-creation/user-creation.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit, AfterViewInit, ViewChild , ElementRef } from '@angul
 import { UserService } from '../../Services/user.service';
 import { User } from '../../interfaces/users';
 import { ActivatedRoute, Router} from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
 import { cardPayment } from '../../interfaces/cardPayment';
 import { OrderCartComponent } from '../../Pages/order-cart/order-cart.component';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-user-creation',
@@ -16,50 +17,35 @@ export class UserCreationComponent implements OnInit {
 
   rows:any = 1
   create:boolean = false
-  paymentMethod:cardPayment[] = []
+  paymentMethod:cardPayment[] = [];
+  stepper!: MatStepper;
+  currentUID = 0;
+
 
   //variable for storing information added to form
   @ViewChild('createForm') form!: NgForm;
 
-  constructor(private creationService:UserService , private router:Router) { }
+  constructor(private userService:UserService , private router:Router, private fb: FormBuilder) { }
   
+  creationFormGroup = this.fb.group({
+    fname: ['', Validators.required],
+    lname: ['', Validators.required],
+    address1: ['', Validators.required],
+    parish: ['', Validators.required],
+    email: ['', Validators.required],
+    cardholder: ['', Validators.required],
+    cardNumber: ['', Validators.required],
+    expiryDate: ['', Validators.required],
+  })
   // Show Create Account
   show() {
     this.create = !this.create;
   }
-
+  
   ngOnInit(): void {
-
-  }
-
-  // Post User Information
-  getUserInformation(value:any) { 
-
-    // Array For PaymentMethod 
-    this.paymentMethod = [{
-      cardNumber: value.cardNumber,
-      expiryDate: value.expiryDate,
-      name: value.cardHolder,
-    }]
-
-    // Object that stores information to be added
-    const userInformation = {
-      id: '',
-      firstName: value.firstName,
-      lastName: value.lastName,
-      email: value.email,
-      paymentMethod: this.paymentMethod
-    }
-
-    // Posting Information to database
-    this.creationService.saveUser(userInformation).subscribe((result) => {
-      console.log(userInformation)
-      this.router.navigateByUrl("/", {skipLocationChange:true})
-      .then(nav => {
-        this.router.navigate(["/", "checkout"])
-      }, err => {
-        console.log(err) // when there's an error
-      });
+    this.userService.lastUserId.subscribe((id)=>{
+      this.currentUID = id + 1;
     })
-  }
+  
+ }
 }
