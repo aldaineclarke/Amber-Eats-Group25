33571c05d83d42ce873a06920fc67d8f3918df66
daniija <daneilhyman12@gmail.com>
2022-07-14 00:29:36 -0500

    added calculation to the order pay page

--- src/app/order-pay/order-pay.component.ts
+++ src/app/order-pay/order-pay.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../Services/cart.service';
 import { CartItem } from '../interfaces/cartItem';
 import { DataService } from '../Services/data.service';
+import { SidesInterface } from '../interfaces/sides';
 import { UserService } from '../Services/user.service';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-order-pay',
   templateUrl: './order-pay.component.html',
   styleUrls: ['./order-pay.component.css']
 })
+
 export class OrderPayComponent implements OnInit {
-
   cartItems: CartItem[] = this.cartService.getCart();
+  sideItems: SidesInterface[] = [];
+  cardAmount: number = this.cartService.getCartCount();
+  cartTotal: number = this.cartService.getCartTotal();
 
-  constructor(private cartService: CartService, private dataService: DataService, private userService: UserService) {
+  constructor(private _formBuilder: FormBuilder, private cartService: CartService, private dataService: DataService, private userService: UserService) {
   }
 
   showAddress:boolean = true;
@@ -28,6 +33,9 @@ export class OrderPayComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.dataService.getAllSides().subscribe((sides) => {
+      this.sideItems = sides;
+    })
   }
 
 }
